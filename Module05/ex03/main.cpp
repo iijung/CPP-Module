e761701d14562f9fdd74eda997d72796c466dd0c
iijung <987kmj4@naver.com>
2023-08-21 22:07:33 +0900

    feat: check terminal (Thanks to @16EB)

--- Module05/ex03/main.cpp
+++ Module05/ex03/main.cpp
@@ -6,14 +6,20 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 23:11:03 by minjungk          #+#    #+#             */
-/*   Updated: 2023/08/13 15:30:27 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/08/21 22:07:26 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "Interface.hpp"
 
 int	main(int argc, char **argv)
 {
+	if (!isatty(STDIN_FILENO))
+	{
+		std::cerr << "Please use termial" << std::endl;
+		return (EXIT_FAILURE);
+	}
 	if (argc != 2)
 	{
 		std::cout << "Usage: " << argv[0] << " <name>" << std::endl;
