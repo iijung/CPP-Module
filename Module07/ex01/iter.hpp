062f07268b46b0e65a995f069f3f6b20b863d8b5
iijung <987kmj4@naver.com>
2023-08-21 23:50:40 +0900

    fix: function parameter is undefined (return, param)

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 01:09:43 by minjungk          #+#    #+#             */
-/*   Updated: 2023/08/19 01:22:30 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/08/21 23:50:29 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 # define __ITER_H__
 # include <iostream>
 
-template <typename T>
-void	iter(T* arr, size_t len, void (*f)(T))
+template <typename T, typename F>
+void	iter(T* arr, size_t len, F f)
 {
 	for (size_t i = 0; i < len; i++)
 		f(arr[i]);
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 01:16:07 by minjungk          #+#    #+#             */
-/*   Updated: 2023/08/19 01:23:02 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/08/21 22:36:35 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	test_int(void)
 	int	arr[] = {1, 2, 3, 4};
 
 	std::cout << "Int: ";
-	iter(arr, sizeof(arr) / sizeof(int), print);
+	iter(arr, sizeof(arr) / sizeof(int), print<int>);
 	std::cout << std::endl;
 }
 
@@ -26,7 +26,7 @@ void	test_float(void)
 	float	arr[] = {1.1, 2.2, 3.3, 4.4};
 
 	std::cout << "Float: ";
-	iter(arr, sizeof(arr) / sizeof(float), print);
+	iter(arr, sizeof(arr) / sizeof(float), print<float>);
 	std::cout << std::endl;
 }
 
@@ -35,7 +35,7 @@ void	test_char(void)
 	char	arr[] = {'h', 'e', 'l', 'l', 'o'};
 
 	std::cout << "Char: ";
-	iter(arr, sizeof(arr) / sizeof(char), print);
+	iter(arr, sizeof(arr) / sizeof(char), print<char>);
 	std::cout << std::endl;
 }
 
